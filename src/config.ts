ecc0502276cc9815d41a9eb7e45728f95ac0f3b5
jsguerra <jose@gcostudios.com>
2023-02-03 20:19:45 +0000

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  website: "https://astro-paper.pages.dev/",
+  author: "Jose",
+  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
+  title: "AstroPaper",
+  ogImage: "astropaper-og.jpg",
+  postPerPage: 3,
+}

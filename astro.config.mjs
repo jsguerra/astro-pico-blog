5922080fa545b68d08924758719cb4d09651a5d1
jsguerra <jose@gcostudios.com>
2023-02-04 21:06:48 +0000

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://site.com",
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()]
 });

f08571d3ae6faeb6a15e0fb2e31a5f8f8395b78b
jsguerra <jose@gcostudios.com>
2023-02-04 14:33:12 +0000

    simplified schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,16 @@
-import { defineCollection } from "astro:content";
-import { blogSchema } from "./_schemas";
+import { z, defineCollection } from "astro:content";
 
 const blog = defineCollection({
-  schema: blogSchema,
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    draft: z.boolean(),
+    author: z.enum(["Jose Guerra"]),
+    featured: z.boolean().optional(),
+    tags: z.array(z.string()).default(["others"]),
+    ogImage: z.string().optional(),
+  })
 });
 
 export const collections = { blog };

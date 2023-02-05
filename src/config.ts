d5bada690f2583e8786c8e525da2334b1228deb6
jsguerra <jose@gcostudios.com>
2023-02-05 20:28:14 +0000

    file updated

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 export const SITE = {
   website: "https://domain.com/",
-  author: "Jose",
+  author: "Jose Guerra",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "CustomAstroBlog",
+  title: "Custom Astro Blog",
   ogImage: "astropaper-og.jpg",
   postPerPage: 3,
 }
